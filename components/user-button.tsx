9fd57c4421a6647af6928a627bc1f7e7f8210bb2
iku <ycyu@milize.co.jp>
2024-05-29 12:11:21 +0900

    add menu button and chang layout of avatar

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -1,44 +1,45 @@
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
-import { Button } from "./ui/button"
-import { auth } from "auth"
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Button } from './ui/button';
+import { auth } from 'auth';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu"
-import { SignIn, SignOut } from "./auth-components"
+} from './ui/dropdown-menu';
+import { SignIn, SignOut } from './auth-components';
+import { Menu } from '@/components/logo/Menu';
 
 export default async function UserButton() {
-  const session = await auth()
-  if (!session?.user) return <SignIn />
+  const session = await auth();
+  if (!session?.user) return <SignIn />;
   return (
-    <div className="flex gap-2 items-center">
-      <span className="hidden text-sm sm:inline-flex" style={{color: "#FEFEFE"}}>
+    <div className='flex gap-2 items-center'>
+      <Avatar className='w-8 h-8'>
+        {session.user.image && (
+          <AvatarImage src={session.user.image} alt={session.user.name ?? ''} />
+        )}
+      </Avatar>
+      <span
+        className='hidden text-sm sm:inline-flex'
+        style={{ color: '#FEFEFE' }}
+      >
         {session.user.email}
       </span>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-            <Avatar className="w-8 h-8">
-              {session.user.image && (
-                <AvatarImage
-                  src={session.user.image}
-                  alt={session.user.name ?? ""}
-                />
-              )}
-              <AvatarFallback>{session.user.email}</AvatarFallback>
-            </Avatar>
+          <Button variant='ghost' className='relative w-8 h-8 rounded-full'>
+          <Menu></Menu>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">
+        <DropdownMenuContent className='w-56' align='end' forceMount>
+          <DropdownMenuLabel className='font-normal'>
+            <div className='flex flex-col space-y-1'>
+              <p className='text-sm font-medium leading-none'>
                 {session.user.name}
               </p>
-              <p className="text-xs leading-none text-muted-foreground">
+              <p className='text-xs leading-none text-muted-foreground'>
                 {session.user.email}
               </p>
             </div>
@@ -49,5 +50,5 @@ export default async function UserButton() {
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
-  )
+  );
 }
